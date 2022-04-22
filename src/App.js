d6fee45b7d9c2978952abf5403de73b81799027e
Harsimran singh <simran.9971290436@gmail.com>
2022-04-22 23:56:49 +0530

    added item in database and updated the alan

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import './App.css';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import { useEffect, useState } from "react";
 import axios from "axios";
-import AppWrapper from "../src/AppWrapper";
-
+// import AppWrapper from "../src/AppWrapper";
+import Navbar from "./components/Navbar";
+import FoodRow from "./components/FoodRow";
+import { BrowserRouter as Router, Switch, Route, Link, useHistory, Redirect } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
+import Checkout from "./components/Checkout";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Footer from "./components/Footer";
+import MainImage from './components/MainImage';
+import Payment from './components/Payment';
+// import { navigate } from 'hookrouter';
+// import { hashHistory } from 'react-router'
 
 
 let App = () => {
+
+  const history = useHistory();
+  console.log(history);
 
   let userCredentials = localStorage.getItem("user logged in");
   let user = JSON.parse(userCredentials);
@@ -19,13 +33,51 @@ let App = () => {
           window.scrollBy(0, window.innerHeight);
 
         } else if (commandData.command === "myItem") {
-          let foodId = commandData.foodId.toString();
-          addToCart(foodId);
+          let { foodId, qty } = commandData.data;
+          foodId = foodId.toString();
+          qty = qty.toString();
+          let noQty = 0;
+          if (qty === "to") {
+            noQty = 2;
+          } else if (qty === "three") {
+            noQty = 3;
+          } else if (qty === "four") {
+            noQty = 4;
+          } else if (qty === "five") {
+            noQty = 5;
+          } else {
+            noQty = 1;
+          }
+          while (noQty-- > 0) {
+            addToCart(foodId);
+          }
         } else if (commandData.command === "removeItem") {
-          let removeId = commandData.foodId.toString();
-          removeFromCart(removeId);
+          let { foodId, qty } = commandData.data;
+          console.log(foodId, qty);
+          foodId = foodId.toString();
+          qty = qty.toString();
+          let noQty = 0;
+          if (qty === "to") {
+            noQty = 2;
+
+          } else {
+            noQty = 1;
+          }
+          while (noQty-- > 0) {
+            console.log("----", noQty)
+            removeFromCart(foodId);
+          }
         } else if (commandData.command === "showcart") {
-          window.scrollBy(0, window.innerHeight);
+          // console.log(route);
+          // console.log(history);
+          // history.push('/checkout');
+          // <Redirect to="/checkout" />
+          // useRedirect('/user', '/dashboard');
+
+          window.location.href = '/checkout';
+          // alanBtnInstance.activate()
+          // window.scrollBy(0, window.innerHeight);'
+          // navigate('/checkout', true);
         }
       }
     });
@@ -52,10 +104,42 @@ let App = () => {
     } catch (err) {
       console.log(err);
     }
+    // document.location.reload(true)
   };
 
   return (
-    <AppWrapper />
+    // <AppWrapper />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Navbar />
+          <MainImage />
+          <FoodRow />
+          <FoodRow />
+          <Footer />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/payment">
+          <Payment />
+        </Route>
+
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/productDetail/:foodId" >
+          <Navbar />
+          <ProductDetail />
+          <Footer />
+        </Route>
+        <Route path="/checkout">
+          {/* <Navbar /> */}
+          <Checkout />
+          <Footer />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
